d86dab0498a548cab94ca203f714a785c944738a
HimanshuDelbatiyan <HimanshuDelbatiyan@gmail.com>
2024-06-24 11:06:56 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+// Importing the Styling for the main component.
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>Welcome Lawde</div>
+    </>
+  )
+}
+
+export default App
