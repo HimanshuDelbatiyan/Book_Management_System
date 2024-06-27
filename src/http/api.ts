42104720f29e7b9e438aa3741a5f12fe01866bec
HimanshuDelbatiyan <HimanshuDelbatiyan@gmail.com>
2024-06-26 22:20:19 -0400

    Add Register Functionality

--- src/http/api.ts
+++ src/http/api.ts
@@ -9,5 +9,10 @@ const api = axios.create({
 
 export const login = async (data:{email:string,password:string}) =>
 {
-    return api.post("/api/users/login",data)
+    return await api.post("/api/users/login",data)
+}
+
+export const register = async (data:{name:string,email:string,password:string})=>
+{
+    return await api.post("/api/users/register",data)
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,10 +2,59 @@ import {Card, CardContent, CardDescription,  CardHeader, CardTitle} from "../com
 import {Label} from "../components/ui/label.tsx";
 import {Input} from "../components/ui/input.tsx";
 import {Button} from "../components/ui/button.tsx";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {useRef} from "react";
+import {useMutation} from "@tanstack/react-query";
+import {register} from "../http/api.ts";
+import {LoaderCircle} from "lucide-react";
 
 
-const RegisterPage = () => {
+const RegisterPage = () =>
+{
+
+    // Using the "useRef" to manipulate the HTML elements directly.
+    const nameRef = useRef<HTMLInputElement>(null)
+    const emailRef = useRef<HTMLInputElement>(null)
+    const passwordRef = useRef<HTMLInputElement>(null)
+
+    const navigate =  useNavigate();
+
+
+
+    // Creating the mutation instance which contains various methods and properties
+    const mutation = useMutation({
+        mutationFn:register,
+        onSuccess: (response) =>
+        {
+            console.log(response.data)
+            navigate("/dashboard/home")
+        }
+    })
+
+
+    const RegisterUser = () =>
+    {
+        const name = nameRef.current?.value;
+        const email = emailRef.current?.value;
+        const password = passwordRef.current?.value;
+
+
+        if(!name || !email || !password)
+        {
+            return alert("None of the field can be left empty")
+        }
+
+        mutation.mutate({name,email,password})
+
+
+    }
+
+
+
+
+
+
+
     return (
         <div className="w-full h-screen items-center bg-zinc-800 bg- flex justify-center bg-opacity-25">
             <Card className="w-full max-w-sm">
@@ -13,19 +62,22 @@ const RegisterPage = () => {
                     <CardTitle className="text-xl">Sign Up</CardTitle>
                     <CardDescription>
                         Enter your information to create an account
+                        <br></br>
+                        {mutation.isError && <span className={"text-red-800 text-center font-bold"}>{mutation.error.message}</span>}
                     </CardDescription>
                 </CardHeader>
                 <CardContent>
                     <div className="grid gap-4">
                         <div className="grid grid-cols-1 gap-4">
                             <div className="grid gap-2">
-                                <Label htmlFor="first-name">Name</Label>
-                                <Input id="first-name" placeholder="Max" required={true}/>
+                                <Label htmlFor="name">Name</Label>
+                                <Input ref={nameRef} id="name" placeholder="Max" required={true}/>
                             </div>
                         </div>
                         <div className="grid gap-2">
                             <Label htmlFor="email">Email</Label>
                             <Input
+                                ref={emailRef}
                                 id="email"
                                 type="email"
                                 placeholder="m@example.com"
@@ -34,10 +86,11 @@ const RegisterPage = () => {
                         </div>
                         <div className="grid gap-2">
                             <Label htmlFor="password">Password</Label>
-                            <Input id="password" type="password"/>
+                            <Input ref={passwordRef} id="password" type="password"/>
                         </div>
-                        <Button type="submit" className="w-full">
-                            Create an account
+                        <Button onClick={RegisterUser} type="submit" className="w-full" disabled={mutation.isPending}>
+                            {mutation.isPending && <span className={"animate-spin"}> <LoaderCircle /> </span>}
+                            <span className={"ml-2"}>Create an account</span>
                         </Button>
                     </div>
                     <div className="mt-4 text-center text-sm">
